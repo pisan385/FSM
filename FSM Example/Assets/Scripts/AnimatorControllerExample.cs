0ed68559a9bd13dd3183b20f97e9af31b34f3ddc
ConradDudziak <conraddudziak@hotmail.com>
2019-04-25 00:28:56 -0700

    FSM Example source code

---
+++ FSM Example/Assets/Scripts/AnimatorControllerExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorControllerExample : MonoBehaviour {
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			animator.SetTrigger("SpaceBarHit");
+		}
+	}
+}
